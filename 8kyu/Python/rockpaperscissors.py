91a4ea2246ec8b8e77fdbec04ac9e7484fa46cc6
morephire <jazzbspec@gmail.com>
2022-12-10 22:30:31 +0100

    New Kata : rock paper scissors (not refactored)

---
+++ 8kyu/Python/rockpaperscissors.py
@@ -0,0 +1,24 @@
+#Very verbose but working solution
+
+def rps(p1, p2):
+
+    if p1 == p2:
+        return 'Draw!'
+
+    elif p1 == 'rock':
+        if p2 == 'paper':
+            return "Player 2 won!"
+        else:
+            return "Player 1 won!"
+            
+    elif p1 == 'scissors':
+        if p2 == 'rock':
+            return "Player 2 won!"
+        else:            
+            return "Player 1 won!"
+    
+    elif p1 == 'paper':
+        if p2 == 'scissors':
+            return "Player 2 won!"
+        else:            
+            return "Player 1 won!"
